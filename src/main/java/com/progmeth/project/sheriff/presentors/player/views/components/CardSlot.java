47b52095eea42edda5a1556cf1e2274b0407975d
msp5382 <meen.sankaew@gmail.com>
2022-12-13 06:24:41 +0700

    chore(ui): rename component names

--- src/main/java/com/progmeth/project/sheriff/presentors/player/views/components/CardSlot.java
+++ src/main/java/com/progmeth/project/sheriff/presentors/player/views/components/CardSlot.java
@@ -7,10 +7,10 @@ public class CardSlot extends StackPane {
     private final static String IMG_EMPTY_SLOT = CardSlot.class.getResource("/com/progmeth/project/sheriff/images/card_empty_slot.png").toString();
 
     public CardSlot() {
-        ImageView EmptySlot = new ImageView(IMG_EMPTY_SLOT);
-        EmptySlot.setFitHeight(93);
-        EmptySlot.setFitWidth(71);
+        ImageView emptySlot = new ImageView(IMG_EMPTY_SLOT);
+        emptySlot.setFitHeight(93);
+        emptySlot.setFitWidth(71);
 
-        getChildren().addAll(EmptySlot);
+        getChildren().addAll(emptySlot);
     }
 }
--- src/main/java/com/progmeth/project/sheriff/presentors/player/views/components/PlayerBoard.java
+++ src/main/java/com/progmeth/project/sheriff/presentors/player/views/components/PlayerBoard.java
@@ -15,55 +15,55 @@ public class PlayerBoard extends BorderPane {
     public PlayerBoard(){
         super.setMinWidth(227);
         super.setMinHeight(310);
-        ImageView PlayerBoardImage = new ImageView(IMG_PLAYER_BOARD);
-        PlayerBoardImage.setFitHeight(310);
-        PlayerBoardImage.setFitWidth(227);
-        super.setCenter(PlayerBoardImage);
-        VBox LeftCardSlot = new VBox();
-        LeftCardSlot.setSpacing(60);
+        ImageView playerBoardImage = new ImageView(IMG_PLAYER_BOARD);
+        playerBoardImage.setFitHeight(310);
+        playerBoardImage.setFitWidth(227);
+        super.setCenter(playerBoardImage);
+        VBox leftCardSlot = new VBox();
+        leftCardSlot.setSpacing(60);
         CardSlot AppleCardSlot = new CardSlot();
         CardSlot CheeseCardSlot = new CardSlot();
-        LeftCardSlot.getChildren().addAll(AppleCardSlot, CheeseCardSlot);
-        LeftCardSlot.setAlignment(Pos.CENTER_RIGHT);
-        LeftCardSlot.setPadding(new javafx.geometry.Insets(0, 0, 0, 140));
-        LeftCardSlot.setTranslateY(-40);
-        VBox RightCardSlot = new VBox();
-        RightCardSlot.setSpacing(60);
+        leftCardSlot.getChildren().addAll(AppleCardSlot, CheeseCardSlot);
+        leftCardSlot.setAlignment(Pos.CENTER_RIGHT);
+        leftCardSlot.setPadding(new javafx.geometry.Insets(0, 0, 0, 140));
+        leftCardSlot.setTranslateY(-40);
+        VBox rightCardSlot = new VBox();
+        rightCardSlot.setSpacing(60);
         CardSlot ChickenCardSlot = new CardSlot();
         CardSlot BreadCardSlot = new CardSlot();
-        RightCardSlot.getChildren().addAll(ChickenCardSlot, BreadCardSlot);
-        RightCardSlot.setAlignment(Pos.CENTER_LEFT);
-        RightCardSlot.setPadding(new javafx.geometry.Insets(0, 140, 0, 0));
-        RightCardSlot.setTranslateY(-40);
+        rightCardSlot.getChildren().addAll(ChickenCardSlot, BreadCardSlot);
+        rightCardSlot.setAlignment(Pos.CENTER_LEFT);
+        rightCardSlot.setPadding(new javafx.geometry.Insets(0, 140, 0, 0));
+        rightCardSlot.setTranslateY(-40);
 
 
-        VBox TopContainer = new VBox();
-        TopContainer.setSpacing(60);
-        HBox TopCardSlot = new HBox();
-        TopCardSlot.getChildren().add(new IllegalCardSlot());
-        TopCardSlot.setAlignment(Pos.CENTER);
+        VBox topContainer = new VBox();
+        topContainer.setSpacing(60);
+        HBox topCardSlot = new HBox();
+        topCardSlot.getChildren().add(new IllegalCardSlot());
+        topCardSlot.setAlignment(Pos.CENTER);
 
         Rotate illegalCardRotate = new Rotate();
         illegalCardRotate.setAngle(90);
         illegalCardRotate.setPivotX(360);
         illegalCardRotate.setPivotY(10);
-        TopCardSlot.getTransforms().add(illegalCardRotate);
+        topCardSlot.getTransforms().add(illegalCardRotate);
 
         Money money = new Money();
         NameTag nameTag = new NameTag();
         nameTag.setName("Meen");
         nameTag.setTranslateX(420);
         nameTag.setTranslateY(10);
-        HBox TopContainerHBox = new HBox();
-        TopContainerHBox.getChildren().addAll(money,nameTag);
-        TopContainer.getChildren().addAll(TopContainerHBox,TopCardSlot);
+        HBox topContainerHBox = new HBox();
+        topContainerHBox.getChildren().addAll(money,nameTag);
+        topContainer.getChildren().addAll(topContainerHBox,topCardSlot);
 
         Hand hand = new Hand();
         hand.setItem(new Item[]{});
 
-        super.setLeft(LeftCardSlot);
-        super.setRight(RightCardSlot);
-        super.setTop(TopContainer);
+        super.setLeft(leftCardSlot);
+        super.setRight(rightCardSlot);
+        super.setTop(topContainer);
         super.setBottom(hand);
 
     }
